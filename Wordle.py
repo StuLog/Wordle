fea2766506e052dcf21ac22a8148854057933302
Stuart Logan <93291680+StuLog@users.noreply.github.com>
2024-06-05 09:28:04 -0400

    Update Wordle.py version 2.0

--- Wordle.py
+++ Wordle.py
@@ -12,8 +12,14 @@ def print_word(user_word, Gp,dp):
         else:
             print(Style.BRIGHT + Back.LIGHTBLACK_EX+ str(i), end = '')
         x = x+1
+def remove_char_at_index(string, index):
+    if index < 0 or index >= len(string):
+        raise IndexError("Index out of range")
+    
+    return string[:index] + "!" + string[index+1:]
 
 gg = 1
+
 file = open("Words.txt", "r")
 content = file.readlines()
 int = random.randint(0,934)
@@ -22,8 +28,10 @@ secret_word = word.strip("\n")
 while(gg):
     great_letters = []
     good_letters = []
+    temp_seceret_word = secret_word
     bad = 1
     print("")
+    correct_spot = False
     while(bad):
         user_word = input("Please give your 5 letter word: ")
         user_word = user_word.lower()
@@ -37,11 +45,19 @@ while(gg):
             gg = 0
             print("You got it!!!")
             break
-        elif(user_word[x] == secret_word[x]):
-            great_letters.append(x)     
-        else:
-            if(secret_word.__contains__(user_word[x])):
-                    good_letters.append(x)
+        elif(user_word[x] == temp_seceret_word[x]):
+            great_letters.append(x)
+            index = temp_seceret_word.index(user_word[x])
+            if(index  >= 0 ):
+                temp_seceret_word = remove_char_at_index(temp_seceret_word, index)
+            correct_spot = True     
+    for x in range(len(secret_word)):
+        if(temp_seceret_word.__contains__(user_word[x]) and gg == 1):
+                good_letters.append(x)
+                index = temp_seceret_word.index(user_word[x])
+                if(index  >= 0 ):
+                    temp_seceret_word = remove_char_at_index(temp_seceret_word, index)
+
     print("---------------------------------------------------------------------")
     print_word(user_word, great_letters,good_letters)
 
